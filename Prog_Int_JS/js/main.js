71c7586d9948b1a0f1479dbb085da30679fc572d
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-07-09 12:45:33 -0300

    first commit

---
+++ Prog_Int_JS/js/main.js
@@ -0,0 +1,23 @@
+﻿let fruta = {
+    nome: "Maça",
+    cor: "Vermelho"
+};
+
+console.log(fruta.nome);
+
+let frutas2 = [
+    {cor: "vermelho", nome: "maçã"},
+    {cor: "roxa", nome: "uva"}
+];
+
+console.log(frutas2[1]);
+
+
+// let lista = ["maça", "pêra", "laranja"];
+
+// lista.push("uva");
+// //lista.pop()
+// console.log(lista[0]);
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+// console.log(lista);
